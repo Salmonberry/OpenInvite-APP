822b154084e07cc25902ce9a010f7ae2dc26e245
chenximing <354018894@qq.com>
2018-12-25 10:41:08 +0800

    no message

---
+++ OpenInvite-APP/src/security/register/RegistrationSuccessfuPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="RegistrationSuccessfu">
+            <text class="RegistrationSuccessfu-text">Registration 
+Successful </text>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'RegistrationSuccessfuPage',
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style scoped>
+.RegistrationSuccessfu {
+    background-color: #57B1E3;
+}
+
+.RegistrationSuccessfu-text {
+    color: #ffffff;
+    font-size: 98px;
+}
+
+</style>
