b50ca36762c665539b3c09ce6d01c78d850750fb
chenximing <354018894@qq.com>
2018-12-25 11:47:07 +0800

    no message

--- OpenInvite-APP/src/security/register/RegisterPage.vue
+++ OpenInvite-APP/src/security/register/RegisterPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="RegisterPage">
          <scroller class="main-list" offset-accuracy="300px">
-          <div class="RegisterPage-retreat"><image  resize="contain" class="retreat" src='/src/image/retreat.png'/></div>
+          <div class="RegisterPage-retreat"><image  resize="contain" class="retreat" src='/src/images/retreat.png'/></div>
           <div class="RegisterPage-title"><text class="RegisterPage-title-text">Create your account</text></div>
           <div class="RegisterPage-content">
                <div class="RegisterPage-list">
@@ -88,15 +88,18 @@
     background-color: #57B1E3;
 }
 .RegisterPage-retreat {
+    position: fixed;
+    top: 20px;
+    left: 26px;
    
-    margin-left: 26px;
-    margin-top: 20px;
     margin-bottom: 20px;
 }
-
 .retreat {
     width: 21px;
     height: 39px;
+}
+.RegisterPage-title {
+margin-top: 66px;
 }
 
 .RegisterPage-title-text {
