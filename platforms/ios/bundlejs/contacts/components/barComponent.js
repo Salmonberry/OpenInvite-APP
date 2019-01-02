b31a8c7c387d23123beca71f2632323104169085
chxu <chxu@liangzhicn.com>
2019-01-02 11:04:02 +0800

    merge

--- platforms/ios/bundlejs/contacts/components/barComponent.js
+++ platforms/ios/bundlejs/contacts/components/barComponent.js
@@ -62,12 +62,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 3:
+/***/ 6:
 /***/ (function(module, exports) {
 
 throw new Error("Module build failed: ReferenceError: Unknown plugin \"component\" specified in \"/Users/lz/Desktop/Openinvite/OpenInvite-APP/.babelrc\" at 0, attempted to resolve relative to \"/Users/lz/Desktop/Openinvite/OpenInvite-APP\"\n    at /Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/options/option-manager.js:180:17\n    at Array.map (<anonymous>)\n    at Function.normalisePlugins (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/options/option-manager.js:158:20)\n    at OptionManager.mergeOptions (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/options/option-manager.js:234:36)\n    at OptionManager.init (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-loader/lib/index.js:50:20)\n    at Object.module.exports (/Users/lz/Desktop/Openinvite/OpenInvite-APP/node_modules/babel-loader/lib/index.js:173:20)");
