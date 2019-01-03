260bea2d9804e387faa317f8735386c74f42a5bd
chenximing <354018894@qq.com>
2019-01-03 10:47:24 +0800

    CreateEventLastPage 完成

--- src/createEvent/CreateEventLastPage.vue
+++ src/createEvent/CreateEventLastPage.vue
@@ -63,14 +63,14 @@
                          <div class="CreateEventLastPage-Public-title-details">
                              <text class="CreateEventLastPage-Public-title-details-text">Allow others join your event? </text>
                              <div ref="test" class="CreateEventLastPage-Public-title-details-btn" @click="slide">
-                                     <text ref="no" class="CreateEventLastPage-Public-title-details-btn-text">NO</text>
+                                     <text v-if="statuss" class="CreateEventLastPage-Public-title-details-btn-text">NO</text>
                                      <div ref="btn" class="CreateEventLastPage-Public-title-details-btn-img"></div>
-                                     <text ref="yes" class="CreateEventLastPage-Public-title-details-btn-text-yes">YES</text>
+                                     <text v-if="pauss" class="CreateEventLastPage-Public-title-details-btn-text-yes">YES</text>
                              </div>
                          </div>
                    </div>
 
-                   <div ref="Showbox" class="CreateEventLastPage-Public-Showbox">
+                   <div v-if="pauss" class="CreateEventLastPage-Public-Showbox">
                         <div class="CreateEventLastPage-Public-Showbox-can">
                                <text class="CreateEventLastPage-Public-Showbox-can-text">Which contacts can attend?</text>
                                <div class="CreateEventLastPage-Public-Showbox-can-select">
@@ -123,8 +123,78 @@
 
               <div class="CreateEventLastPage-Location">
                   <text class="CreateEventLastPage-Location-title">Location</text>
+                  <div class="CreateEventLastPage-Location-content">
+                      <input type="text" placeholder="Search" class="CreateEventLastPage-Location-input"/>
+                      <image class="CreateEventLastPage-Location-img" src="/src/images/maps-search.png"/>
+                  </div>
               </div>
 
+              <div class="separate"></div>
+
+              <div class="CreateEventLastPage-Location-CoverImage">
+                  <text class="CreateEventLastPage-Location-CoverImage-title">Cover Image</text>
+                  <image class="CreateEventLastPage-Location-CoverImage-img" src="/src/images/createEventLastPage-Location-CoverImage-img.png" />
+                  <div class="CreateEventLastPage-Location-CoverImage-btn">
+                          <image class="CreateEventLastPage-Location-CoverImage-btn-img" src="/src/images/refresh-button.png"/>
+                          <text class="CreateEventLastPage-Location-CoverImage-btn-text">New random photo</text>
+                  </div>
+                  <div class="CreateEventLastPage-Location-CoverImage-btn">
+                          <image class="CreateEventLastPage-Location-CoverImage-btn-img-pic" src="/src/images/gallery.png"/>
+                          <text class="CreateEventLastPage-Location-CoverImage-btn-text">Choose From Your Gallery</text>
+                  </div>
+                  <div class="CreateEventLastPage-Location-CoverImage-btn">
+                          <image class="CreateEventLastPage-Location-CoverImage-btn-imgphoto" src="/src/images/photo-camera.png"/>
+                          <text class="CreateEventLastPage-Location-CoverImage-btn-text">Take A Photo</text>
+                  </div>
+              </div>
+               <div class="separate"></div>
+               <div class="CreateEventLastPage-Location-Other">
+                    <text class="CreateEventLastPage-Location-Other-title">Other</text>
+
+                    <div class="CreateEventLastPage-Location-Other-list">
+                        <text class="CreateEventLastPage-Location-Other-list-text">Repeat :</text>
+                        <div class="CreateEventLastPage-Location-Other-list-select">
+                            <text class="CreateEventLastPage-Location-Other-list-select-text">No</text>
+                            <image class="CreateEventLastPage-Location-Other-list-select-img" src="/src/images/select.png"/>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="CreateEventLastPage-Location-Other-list">
+                        <text class="CreateEventLastPage-Location-Other-list-text">Show As :</text>
+                        <div class="CreateEventLastPage-Location-Other-list-select">
+                            <text class="CreateEventLastPage-Location-Other-list-select-text">Busy</text>
+                            <image class="CreateEventLastPage-Location-Other-list-select-img" src="/src/images/select.png"/>
+                        </div>
+                    </div>
+                    
+                    <div class="CreateEventLastPage-Location-Other-list">
+                        <text class="CreateEventLastPage-Location-Other-list-text">Alert:</text>
+                        <div class="CreateEventLastPage-Location-Other-list-select">
+                            <text class="CreateEventLastPage-Location-Other-list-select-text">No</text>
+                            <image class="CreateEventLastPage-Location-Other-list-select-img" src="/src/images/select.png"/>
+                        </div>
+                    </div>
+                    
+                    <div class="CreateEventLastPage-Location-Other-list">
+                        <text class="CreateEventLastPage-Location-Other-list-text">Event Fee </text>
+                         <text class="CreateEventLastPage-Location-Other-list-text">$0 /person </text>
+                    </div>
+                    <text class="CreateEventLastPage-Location-Other-What">What will you be doing?</text>
+               </div>
+            <div class="separate"></div>
+            <div class="CreateEventLastPage-Location-Check">
+                <text class="CreateEventLastPage-Location-Check-title">Item Check List</text>
+                <text class="CreateEventLastPage-Location-Check-titleIntroduction">What should people bring to your event? </text>
+                <text class="CreateEventLastPage-Location-Check-selecttext">Item 1</text>
+                <div class="CreateEventLastPage-Location-Check-label">
+                    <image class="CreateEventLastPage-Location-Check-label-img" src="/src/images/createEventLastPage-labeladd.png"/>
+                    <text class="CreateEventLastPage-Location-Check-label-text">Item </text>
+                </div>
+            </div>
+               <div class="CreateEventLastPage-Location-Submit">
+            <text class="CreateEventLastPage-Location-Submit-text">Submit</text>
+        </div>
         </scroller>
     </div>
 </template>
@@ -134,16 +204,15 @@ const animation = weex.requireModule('animation')
     export default {
         data() {
             return {
-                statuss:true
+                statuss:true,
+                pauss:false
             }
         },
         methods:{
             slide(){
                 var testEl = this.$refs.test;
                 var btn = this.$refs.btn;
-                var no = this.$refs.no;
-                var yes = this.$refs.yes;
-                var Showbox = this.$refs.Showbox;
+               
                 if(this.statuss){
 
                     animation.transition(testEl, {
@@ -161,24 +230,10 @@ const animation = weex.requireModule('animation')
                     timingFunction: 'ease',
                     });
 
-                    animation.transition(no, {
-                    styles: {
-                          display: 'none'
-                    },
-                    });
+                 
 
-                    animation.transition(yes, {
-                    styles: {
-                          display: 'block'
-                    },
-                    });
-                    animation.transition(Showbox, {
-                    styles: {
-                          display: 'block'
-                    },
-                    });
-
-                    this.statuss=false;
+                    this.statuss=!this.statuss;
+                      this.pauss=!this.pauss;
                 }else {
                     
                     
@@ -197,25 +252,10 @@ const animation = weex.requireModule('animation')
                     timingFunction: 'ease',
                     });
 
-                    animation.transition(no, {
-                    styles: {
-                          display: 'block'
-                    },
-                    });
+                   
 
-                    animation.transition(yes, {
-                    styles: {
-                          display: 'none'
-                    },
-                    });
-
-                    animation.transition(Showbox, {
-                    styles: {
-                          display: 'none'
-                    },
-                    });
-
-                    this.statuss=true;
+                  this.statuss=!this.statuss;
+                   this.pauss=!this.pauss;
 
                 }
 
@@ -383,12 +423,12 @@ const animation = weex.requireModule('animation')
      .CreateEventLastPage-Public-title-details-btn-text-yes {
         color: #ffffff;
         font-size: 18px;
-        display: none;
+        
         margin-left: 50px;
     }
     .CreateEventLastPage-Public-Showbox {
         padding-bottom: 35px;
-        display: none;
+        
     }
     .CreateEventLastPage-Public-Showbox-can {
         flex-direction: row;
@@ -475,5 +515,174 @@ const animation = weex.requireModule('animation')
         padding-top: 35px;
         padding-left: 30px;
         padding-right: 30px;
+    }
+    .CreateEventLastPage-Location-title {
+        color: #313131;
+        font-size: 50px;
+        margin-bottom: 20px;
+    }
+    .CreateEventLastPage-Location-content {
+        flex-direction: row;
+        align-items: center ;
+        justify-content:space-between;
+        padding-left: 12px;
+        padding-right: 12px;
+        padding-bottom: 12px;
+        padding-top: 12px;
+        background-color: #F4F4F4;
+        border-radius:18px;
+        height: 62px;
+    }
+    .CreateEventLastPage-Location-input {
+        font-size:25px;
+        width: 300px;
+        height: 62px;
+        color: #242424;
+        flex: 1;
+    }
+    .CreateEventLastPage-Location-img {
+        width: 26px;
+        height: 28px;
+    }
+    .CreateEventLastPage-Location-CoverImage {
+        padding-top: 35px;
+        padding-bottom: 35px;
+        padding-left: 30px;
+        padding-right: 30px;
+    }
+    .CreateEventLastPage-Location-CoverImage-title {
+        color: #313131;
+        font-size: 50px;
+    }
+    .CreateEventLastPage-Location-CoverImage-img {
+        margin-top: 18px;
+        margin-bottom: 18px;
+        width: 700px;
+        height: 750px;
+    }
+    .CreateEventLastPage-Location-CoverImage-btn {
+        flex-direction: row;
+        align-items: center ;
+        justify-content:center;
+        background-color: #57B1E3;
+        height: 88px;
+        margin-bottom: 18px;
+    }
+    .CreateEventLastPage-Location-CoverImage-btn-img {
+        width: 24px;
+        height: 26px;  
+        margin-right: 7px;
+    }
+    .CreateEventLastPage-Location-CoverImage-btn-img-pic {
+        width: 34px;
+        height: 34px;
+         margin-right: 7px;
+    }
+    .CreateEventLastPage-Location-CoverImage-btn-imgphoto {
+        width: 38px;
+        height: 30px;
+        margin-right: 7px;
+    }
+    .CreateEventLastPage-Location-CoverImage-btn-text {
+        color: #ffffff;
+        font-size: 30px;
+    }
+
+    .CreateEventLastPage-Location-Other {
+        padding-top: 35px;
+        padding-bottom: 35px;
+    }
+    .CreateEventLastPage-Location-Other-title {
+       padding-left: 30px;
+        color: #313131;
+        font-size: 50px;
+    }
+    .CreateEventLastPage-Location-Other-list {
+        flex-direction: row;
+        align-items: center ;
+        justify-content:space-between;
+        padding-left: 30px;
+        padding-right: 30px;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        border-bottom-width: 2px;
+        border-bottom-color: rgba(112, 112, 112, .34);
+        border-bottom-style: solid;
+    }
+    .CreateEventLastPage-Location-Other-list-text {
+        color: #707070;
+        font-size: 32px;
+    }
+    .CreateEventLastPage-Location-Other-list-select {
+        flex-direction: row;
+        align-items: center ;
+        justify-content:space-between;
+    }
+    .CreateEventLastPage-Location-Other-list-select-text {
+        color: #707070;
+        font-size: 32px;
+    }
+    .CreateEventLastPage-Location-Other-list-select-img {
+        width: 22px;
+        height: 20px;
+        margin-left: 18px;
+    }
+    .CreateEventLastPage-Location-Other-What {
+         color: #707070;
+        font-size: 32px;
+        padding-left: 30px;
+        padding-top: 30px;
+    }
+    .CreateEventLastPage-Location-Check {
+         padding-top: 35px;
+        padding-bottom: 201px;
+          padding-left: 30px;
+        padding-right: 30px;
+    }
+    .CreateEventLastPage-Location-Check-title {
+        color: #313131;
+        font-size: 50px;
+    }
+    .CreateEventLastPage-Location-Check-titleIntroduction {
+        color: #707070;
+        font-size: 32px;
+        margin-bottom: 66px;
+    }
+    .CreateEventLastPage-Location-Check-selecttext {
+        padding-bottom: 10px;
+        color: rgba(112, 112, 112, .5);
+        font-size: 32px;
+        border-bottom-width: 2px;
+        border-bottom-color: rgba(112, 112, 112);
+        border-bottom-style: solid;
+        margin-bottom: 16px;
+    }
+    .CreateEventLastPage-Location-Check-label {
+         flex-direction: row;
+        align-items: center ;
+    }
+    .CreateEventLastPage-Location-Check-label-img {
+        width: 32px;
+        height: 30px;
+    }
+    .CreateEventLastPage-Location-Check-label-text{
+        color: #57B1E3;
+        font-size: 32px;
+    }
+    .CreateEventLastPage-Location-Submit {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 750px;
+        height: 166px;
+        background-color: #57B1E3;
+         flex-direction: row;
+        align-items: center ;
+        justify-content:center;
+        
+    }
+    .CreateEventLastPage-Location-Submit-text {
+          color: #ffffff;
+        font-size: 30px;
     }
 </style>
